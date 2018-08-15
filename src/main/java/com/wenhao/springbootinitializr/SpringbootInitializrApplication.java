f45e7f3087bc0b8ac01055cdddcb71daa49078c3
wenhao543 <543161890@qq.com>
2018-08-16 00:15:18 +0800

    第一次提交

---
+++ src/main/java/com/wenhao/springbootinitializr/SpringbootInitializrApplication.java
@@ -0,0 +1,12 @@
+package com.wenhao.springbootinitializr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInitializrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInitializrApplication.class, args);
+	}
+}
