ecafd901fc301c4d8b79ad5b8a490b78da6d16f1
liuwenhao <543161890@qq.com>
2018-08-17 17:07:40 +0800

    logback日志插件引入

--- src/main/java/com/wenhao/test/controller/TestController.java
+++ src/main/java/com/wenhao/test/controller/TestController.java
@@ -15,5 +15,13 @@ public class TestController {
 		user.setAddress("重庆万州");
 		return user;
 	}
+	
+	@RequestMapping("/errorTest")
+	public String errorTest() throws Exception {
+		if(1==1) {
+			throw new Exception("不好意思抛一段异常看！");
+		}
+		return "hello";
+	}
 
 }
